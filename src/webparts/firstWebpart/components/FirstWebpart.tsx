2062912e469bd23ca88d3b86503ea8186d0923b7
vijaythapakdata <vijaythapak24@gmail.com>
2025-09-26 11:41:03 +0530

    first commit

---
+++ src/webparts/firstWebpart/components/FirstWebpart.tsx
@@ -0,0 +1,117 @@
+import * as React from 'react';
+// import styles from './FirstWebpart.module.scss';
+import type { IFirstWebpartProps } from './IFirstWebpartProps';
+import { TextField,Dropdown ,ComboBox, ChoiceGroup,Slider,Toggle,Rating,  Checkbox, PrimaryButton, DefaultButton, SearchBox} from '@fluentui/react';
+
+const FirstWebpart:React.FC<IFirstWebpartProps>=(props)=>{
+  return(
+    <>
+    <p> I am learning <strong>spfx</strong></p>
+    <hr/>
+    <h4>NEW FORM</h4>
+    <hr/>
+    <form>
+      <SearchBox
+      placeholder='search here.....'
+      iconProps={{iconName:'Search'}}
+      />
+      <br/>
+<TextField
+label='Name'
+placeholder='Enter value here'
+
+/>
+      <TextField
+      label='Email'
+      type='email'
+      placeholder='Enter email here'
+      iconProps={{iconName:'mail'}}
+      />
+      <TextField
+label='Password'
+// placeholder='Enter value here'
+type='password'
+canRevealPassword={true}
+
+/>
+<TextField
+label='Upload File'
+// placeholder='Enter value here'
+type='file'
+
+
+/>
+<TextField
+label='Salary'
+// placeholder='Enter value here'
+prefix='$'
+suffix='USD'
+
+/>
+<Dropdown
+label='Department'
+options={[
+  {key:'HR',text:'HR'},
+  {key:'IT',text:'IT'},
+  {key:'Finance',text:'Finance'},
+]}
+placeholder='--select--'
+multiSelect
+/>
+<ComboBox
+label='Skills'
+options={[
+  {key:'javascript',text:'JavaScript'},
+  {key:'reactjs',text:'ReactJS'},
+  {key:'angular',text:'Angular'},
+  {key:'vuejs',text:'VueJS'},
+]}
+allowFreeform
+autoComplete='on'
+multiSelect
+/>
+<ChoiceGroup
+label='Gender'
+options={[
+  {key:'Male',text:'Male'},
+  {key:'Female',text:'Female'}
+]}
+/>
+<Slider
+label='Score'
+min={1}
+max={100}
+step={1}
+/>
+<Toggle
+label='Yes/No'
+/>
+<Rating
+    max={5}
+        // size={L}
+        defaultRating={1}
+        ariaLabel="Large stars"
+        ariaLabelFormat="{0} of {1} stars"
+/>
+<TextField
+label='Comments'
+multiline
+rows={5}
+iconProps={{iconName:'comment'}}
+/>
+<Checkbox
+label='I accept terms and conditions'
+/>
+<br/>
+<PrimaryButton
+text='Save' iconProps={{iconName:'Save'}}
+/>
+&nbsp; &nbsp;&nbsp; &nbsp;
+<DefaultButton
+text='Cancel' iconProps={{iconName:'Cancel'}}
+/>
+    </form>
+    </>
+  )
+}
+export default FirstWebpart;
---
+++ src/webparts/fluentUi/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IFluentUiWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'FluentUiWebPartStrings' {
+  const strings: IFluentUiWebPartStrings;
+  export = strings;
+}
