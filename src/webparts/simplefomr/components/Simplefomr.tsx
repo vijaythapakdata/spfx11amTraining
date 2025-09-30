c3cc6501a47dd5f31705d57009b4b2ba61cc5c26
vijaythapakdata <vijaythapak24@gmail.com>
2025-09-30 11:49:59 +0530

    restapi

--- src/webparts/simplefomr/components/Simplefomr.tsx
+++ src/webparts/simplefomr/components/Simplefomr.tsx
@@ -6,6 +6,7 @@ import {Web} from "@pnp/sp/presets/all";
 import "@pnp/sp/items";
 import "@pnp/sp/lists";
 import { Dialog } from '@microsoft/sp-dialog';
+import { PrimaryButton, TextField } from '@fluentui/react';
 
 const Simplefomr:React.FC<ISimplefomrProps>=(props)=>{
   const [formData,setFormdata]=React.useState<ISimpleFomrState>({
@@ -17,7 +18,7 @@ try{
 let web=Web(props.siteurl); // it will capture the site url
 let list=web.lists.getByTitle(props.ListName);
 let item=await list.items.add({
-  Title:formData.Name
+Title:formData.Name
 });
 Dialog.alert(`Item has been created successfully with ID :${item.data.Id}`);
 setFormdata({
@@ -29,9 +30,26 @@ console.error("Erorr",err);
 Dialog.alert(`Erorr while creating the item : ${err}`);
 }
   }
+
+  //event handling 
+  const FormEvent=(fieldvalue:keyof ISimpleFomrState,value:string)=>{
+    // setFormdata(prev=>({...prev,[fieldvalue]:value}))
+    setFormdata(prev=>({...prev,[fieldvalue]:value}))
+  }
   
   return(
-    <></>
+    <>
+    <TextField
+    label='Name'
+    value={formData.Name}
+    onChange={(_,value)=>FormEvent("Name",value||"")}
+    />
+    <br/>
+    <PrimaryButton text='Save'
+    onClick={createItem}
+    iconProps={{iconName:'save'}}
+    />
+    </>
   )
 }
 export default Simplefomr;
